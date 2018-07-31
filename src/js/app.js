f7a20ce63af30b766f92fdea63b4f1c1a7d335a2
Vitalii Yevtushenko <monarhnewage@gmail.com>
2018-07-31 08:19:59 +0300

    js

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,7 @@
 import 'slick-carousel';
+import './lib/scroll.js';
+import './lib/gallery.js';
+
 
 $('.js-hero').slick({
   nextArrow: '.slider__next',
---
+++ src/js/lib/scroll.js
@@ -0,0 +1,6 @@
+import ScrollReveal from 'scrollreveal';
+
+
+window.sr = ScrollReveal({ reset: true });
+sr.reveal('.post, .quote, .post-link', { duration: 2000 });
+
